b53a3418366afec413a91a5ea5fc35c748636732
Ruben Rosenmeyer <ruben.rosenmeyer@gmail.com>
2026-07-08 02:01:32 +0200

    switched to parametrized testfiles

--- src/bench_medium_NoArchive/test_simple2.py
+++ src/bench_medium_NoArchive/test_simple2.py
@@ -1,52 +1,8 @@
-def test_simple_2():
-    index = 0
-    for i in range(1000000):
-        index += 1
-    assert index == 1000000
+import pytest
 
-def test_simple_3():
-    index = 0
-    for i in range(1000000):
-        index += 1
-    assert index == 1000000
 
-def test_simple_4():
-    index = 0
-    for i in range(1000000):
-        index += 1
-    assert index == 1000000
-
-def test_simple_5():
-    index = 0
-    for i in range(1000000):
-        index += 1
-    assert index == 1000000
-
-def test_simple_6():
-    index = 0
-    for i in range(1000000):
-        index += 1
-    assert index == 1000000
-
-def test_simple_7():
-    index = 0
-    for i in range(1000000):
-        index += 1
-    assert index == 1000000
-
-def test_simple_8():
-    index = 0
-    for i in range(1000000):
-        index += 1
-    assert index == 1000000
-
-def test_simple_9():
-    index = 0
-    for i in range(1000000):
-        index += 1
-    assert index == 1000000
-
-def test_simple_10():
+@pytest.mark.parametrize("case", range(10))
+def test_simple(case):
     index = 0
     for i in range(1000000):
         index += 1
